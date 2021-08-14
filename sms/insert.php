f3d774d41718a2d5cdba4d0e15fe3eaccfb195c7
Nehal Patel <patelnehal4u@gmail.com>
2021-08-14 15:35:44 +0530

    new fields added in database

--- sms/insert.php
+++ sms/insert.php
@@ -12,12 +12,21 @@
 
     if(!empty($_POST)){
 
+        // echo "<pre>";
+        // print_r($_POST);exit;
+
         require_once 'includes/functions.php';        
 
         $student_name = senitize_input( $_POST['student_name'] );
         $student_email = senitize_input( $_POST['student_email'] );
         $student_mobile = senitize_input( $_POST['student_mobile'] );
         $student_city = senitize_input( $_POST['student_city'] );
+
+        $gender = $_POST['gender'];
+        $hobbies = implode(',', $_POST['hobbies']);
+        // echo $hobbies;exit;
+        $course = senitize_input( $_POST['course'] );
+        $address = senitize_input( $_POST['address'] );
 
         require_once 'includes/validation.php';
 
@@ -26,7 +35,9 @@
             //DB Insert
             require_once 'includes/db.php';
 
-            $insert_sql = 'INSERT INTO students (name,email,mobile,city) VALUES ("'. $student_name .'", "'. $student_email .'", "'. $student_mobile .'", "'. $student_city  .'")';
+            $insert_sql = 'INSERT INTO students (name,email,gender,hobbies,course,address,mobile,city) VALUES ("'. $student_name .'", "'. $student_email .'", "'. $gender .'","'. $hobbies .'","'. $course .'","'. $address .'", "'. $student_mobile .'", "'. $student_city  .'")';
+
+            // echo $insert_sql;exit;
 
             mysqli_query($connection, $insert_sql); // PHP 7,8
 
@@ -81,6 +92,37 @@
         <input type="text" class="form-control" id="student_city" name="student_city" placeholder="Student City" value="<?php echo isset($_POST['student_city']) ? $_POST['student_city'] : '' ?>">
         
     </div>
+
+
+    <div class="radio">
+        <label for="gender">Gender</label>
+
+        <label><input type="radio" name="gender" value="1"> Male</label>
+        <label><input type="radio" name="gender" value="0"> Female</label>        
+    </div>
+
+    <div class="form-group">        
+        <label for="gender">Hobbies</label>
+        <label><input type="checkbox" name="hobbies[]" value="sports"> Sports</label>
+        <label><input type="checkbox" name="hobbies[]" value="reading"> Reading</label>
+        <label><input type="checkbox" name="hobbies[]" value="writing"> Writing</label>
+    </div>
+
+    <div class="form-group">        
+        <label for="gender">Course</label>
+        <select name="course">
+            <option value="bca">BCA</option>
+            <option value="bcom">BCom</option>
+            <option value="bba">BBA</option>
+        </select>
+    </div>
+
+     <div class="form-group">        
+        <label for="gender">Address</label>
+        <textarea class="form-control" rows="3" name="address"></textarea>
+    </div>
+
+
     
     <button type="submit" class="btn btn-primary">Submit</button>
 </form>
--- sms/update.php
+++ sms/update.php
@@ -9,12 +9,28 @@
 
     if(!empty($_POST)){
 
+        require_once 'includes/functions.php';
+
+        $student_id = senitize_input( $_POST['student_id'] );
+        $student_name = senitize_input( $_POST['student_name'] );
+        $student_email = senitize_input( $_POST['student_email'] );
+        $student_mobile = senitize_input( $_POST['student_mobile'] );
+        $student_city = senitize_input( $_POST['student_city'] );
+
+        $gender = $_POST['gender'];
+        $hobbies = implode(',', $_POST['hobbies']);
+        // echo $hobbies;exit;
+        $course = senitize_input( $_POST['course'] );
+        $address = senitize_input( $_POST['address'] );
+
         require_once 'includes/validation.php';
 
 
         if( !$is_error ){
             
-            $update_sql = 'UPDATE `students` SET `name` = "'. $_POST['student_name'] .'", `email` = "'. $_POST['student_email'] .'",`mobile` = "'. $_POST['student_mobile'] .'",`city` = "'. $_POST['student_city'] .'" WHERE `students`.`id` = '. $_POST['student_id'];
+            $update_sql = 'UPDATE `students` SET `name` = "'. $student_name .'", `email` = "'. $student_email .'", `gender` = "'. $gender .'", `hobbies` = "'. $hobbies .'", `course` = "'. $course .'", `address` = "'. $address .'",`mobile` = "'. $student_mobile .'",`city` = "'. $student_city .'" WHERE `students`.`id` = '. $student_id;
+
+            // echo $update_sql;exit;
 
             mysqli_query($connection, $update_sql); // PHP 7,8
 
@@ -77,6 +93,40 @@
         <label for="student_city">Student City</label>
         <input type="text" class="form-control" id="student_city" name="student_city" placeholder="Student City"  value="<?php echo $row['city'] ?>">
         
+    </div>
+
+    <div class="radio">
+        <label for="gender">Gender</label>
+
+        <label><input type="radio" name="gender" value="1" <?php echo $row['gender']==1?"checked":''; ?> > Male</label>
+        <label><input type="radio" name="gender" value="0" <?php echo $row['gender']==0?"checked":''; ?>> Female</label>
+    </div>
+
+    <div class="form-group">        
+        
+        <?php 
+            $hobbies = $row['hobbies'];  //reading,writing,sports
+            $hobbies = explode(",", $hobbies);
+        ?>
+
+        <label for="gender">Hobbies</label>
+        <label><input type="checkbox" name="hobbies[]" value="sports" <?php echo in_array('sports', $hobbies)?'checked':''; ?>> Sports</label>
+        <label><input type="checkbox" name="hobbies[]" value="reading" <?php echo in_array('reading', $hobbies)?'checked':''; ?>> Reading</label>
+        <label><input type="checkbox" name="hobbies[]" value="writing" <?php echo in_array('writing', $hobbies)?'checked':''; ?>> Writing</label>
+    </div>
+
+    <div class="form-group">        
+        <label for="gender">Course</label>
+        <select name="course">
+            <option value="bca" <?php echo $row['course']=='bca'?'selected':''; ?>>BCA</option>
+            <option value="bcom" <?php echo $row['course']=='bcom'?'selected':''; ?>>BCom</option>
+            <option value="bba" <?php echo $row['course']=='bba'?'selected':''; ?>>BBA</option>
+        </select>
+    </div>
+
+     <div class="form-group">        
+        <label for="gender">Address</label>
+        <textarea class="form-control" rows="3" name="address"><?php echo $row['address']; ?></textarea>
     </div>
     
     <button type="submit" class="btn btn-primary">Submit</button>
